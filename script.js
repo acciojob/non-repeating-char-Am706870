f4325f3f00781054386e3ebf57dca8848715f798
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-05-19 18:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function firstNonRepeatedChar(str) {
+	let charcount = {};
+	for(let char of str){
+		char = char.toLowerCase();
+		charcount[char] = (charcount[char] || 0)+1;
+	}
+
+	for(char of str){ 
+		if(charcount[char.toLowerCase()]===1){
+			return char;
+		}
+	}
+	return null;
  // Write your code here
 }
 const input = prompt("Enter a string");
